ddc8ba6e3682f1a1d8546467c2513ad0ca6b969e
hassanzhd <hassanzhd2000@gmail.com>
2020-04-27 13:24:10 +0500

    PORT 5000 removed

--- www/server.js
+++ www/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("../app");
 
-let PORT = process.env.PORT | 5000;
+let PORT = process.env.PORT;
 let server = http.createServer(app);
 
 server.listen(PORT, () => {
