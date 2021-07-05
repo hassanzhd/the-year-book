1c99fa996d06deff99727b94099d5d746b3ea080
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-07-05 21:18:44 +0500

    setup next.js with typescript

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>The Year Book | Login or Sign up </title>
+        <meta
+          name="description"
+          content="Log into the year book, to create and sign year books."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <a href="#">gage</a>
+    </div>
+  );
+}
