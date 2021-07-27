917485511f506250cbe37e1ea619b755f8db54d1
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-07-28 01:12:31 +0500

    added helper provider for user helper functions

---
+++ api/src/user/user.helper.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class UserHelper {
+  uploadFile(__file: Express.Multer.File) {
+    console.log(__file);
+  }
+
+  generateVerificationHash(): string {
+    const verificationHash = randomBytes(30).toString('hex');
+    return verificationHash;
+  }
+}
